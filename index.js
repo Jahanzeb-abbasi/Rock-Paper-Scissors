db4ae9e6a9d6a773270ec0836a7fb921dd51b197
Jahanzeb <jahanzebabbasi49@gmail.com>
2023-02-01 18:48:12 +0500

    Fixed game()

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-function getComputerChoice() {
-    const moves = ['Rock','Paper','Scissor'];
-    let chosenMove = moves[Math.floor(Math.random()*3)]
-    return chosenMove;
-}
-
 let playerScore = 0;
 let computerScore = 0;
 
+// Rock, Paper, Scissors Logic 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a draw!"
@@ -37,20 +32,51 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Fix the Prompt when asking for input.
-//Make smaller functions - Capitalize, Input
+// Get Player Choice 
+function getPlayerChoice() {
+    let playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+    return playerSelection;
+}
+
+// Get Computer Choice 
+function getComputerChoice() {
+    const moves = ['Rock','Paper','Scissor'];
+    let chosenMove = moves[Math.floor(Math.random()*3)]
+    return chosenMove;
+}
+
+// Decide the Game Winner 
+function gameWinner() {
+    if (playerScore > computerScore) {
+        return "You Win!"
+    }
+    else if(playerScore == computerScore) {
+        return "It's a draw"
+    }
+    else {
+        return "You Lose!"
+    }
+}
 
 function game() {
+    
+    // 5 Round Game 
     for (i = 0; i < 5; i++) {
-        playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+
+        let playerSelection = getPlayerChoice();
         let computerSelection = getComputerChoice();
+
+        // Round Status 
         console.log(`Round ${i+1}`)
         console.log(`Player Choice: ${playerSelection}`)
         console.log(`Computer Choice: ${computerSelection}`)
         console.log(playRound(playerSelection,computerSelection))
         console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`)
     }
+    // Outputs the Final Result 
+    console.log(`Final Score: Player - ${playerScore} | Computer - ${computerScore}`)
+    console.log(gameWinner());
 }
 
 game();
